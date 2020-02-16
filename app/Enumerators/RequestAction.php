0540fccb4456ea70832aa3aca819a691d1bd5145
Sidney Oliveira <oliveira.sidney96@gmail.com>
2020-02-16 20:12:10 -0300

    Create enum to request actions

---
+++ app/Enumerators/RequestAction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enumerators;
+
+final class RequestAction
+{
+    const INDEX = 'index';
+    const STORE = 'store';
+    const SHOW = 'show';
+    const UPDATE = 'update';
+    const DELETE = 'delete';
+}
