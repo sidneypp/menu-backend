b98a2c05dc75bd6d3501190c3d9f2a3e77ac34eb
Sidney Oliveira <oliveira.sidney96@gmail.com>
2020-02-16 20:36:12 -0300

    Add default respondWith function in base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function respondWith($body, int $status = Response::HTTP_OK): Response
+    {
+        if (is_string($body)) {
+            return response(['message' => trans($body)], $status);
+        }
+
+        return response($body, $status);
+    }
 }
