a933179317a8eaba41f59ae1f95ebb1044f81e84
Sidney Oliveira <oliveira.sidney96@gmail.com>
2020-02-16 21:46:10 -0300

    Create custom messages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'customer_delete_success' => 'Customer has been successfully deleted.',
+
+    'order_delete_success' => 'Order has been successfully deleted.',
+
+    'record_not_found' => ':model with id :id wasn\'t found.',
+];
