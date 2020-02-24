ee0f4df04a696579bb828b6d69e96b4afbac2512
Sidney Oliveira <oliveira.sidney96@gmail.com>
2020-02-23 21:50:08 -0300

    Change default language to pt-BR

---
+++ resources/lang/pt-BR/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'customer_delete_success' => 'O cliente foi excluído com sucesso.',
+    'order_delete_success' => 'O pedido foi excluído com sucesso.',
+    'record_not_found' => ':model com id :id não foi encontrado'
+];
